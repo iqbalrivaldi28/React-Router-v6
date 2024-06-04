886a716b12ab83f44788b93950a28608710994dd
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-06-04 14:41:07 +0700

    feat: add routing basic concept

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import Navigation from './components/Navigation';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Cars from './pages/Cars';
+import Contact from './pages/Contact';
+import About from './pages/About';
 
 function App() {
   return (
     <>
-      <></>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
   );
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
+    <>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/cars">Cars</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </>
   )
 }
 
