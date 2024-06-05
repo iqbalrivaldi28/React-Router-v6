4ef99283ad6267a0249c46154a1ed5c510a05f42
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-06-05 15:25:14 +0700

    feat: add useNavigate concept

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
+import React from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
-  return (
-    <Navigate to= '/' />
-  )
-}
+  const navigate = useNavigate();
 
-export default NotFound
+  return (
+    // <Navigate to= '/' />
+
+    <>
+      <div>Not Found</div>
+      <button onClick={() => navigate('/')}>Back To Home</button>
+    </>
+  );
+};
+
+export default NotFound;
+
+//? Note: penggunaan useNavigate itu untuk handle session login, jadi kalau waktu 60 menit dia diarahkan ke login lagi
